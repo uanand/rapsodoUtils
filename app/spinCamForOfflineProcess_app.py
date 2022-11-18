4b38e598555c29bbe5235c6fa59cd5cafe2ca260
Utkarsh Anand <utkarsh.anand@rapsodo.com>
2022-11-18 10:47:16 +0800

    Application to convert spin camera mkv to raw.

--- app/spinCamForOfflineProcess_app.py
+++ app/spinCamForOfflineProcess_app.py
@@ -4,10 +4,16 @@ import sys
 sys.path.append(os.path.abspath('../resources'))
 
 import imageConvert
+import fileIO
 
 riffile = sys.argv[1]
-spinMkvFile = riffile + "/spin_server.mkv"
-spinRawFile = riffile + "/spin_server.raw"
+if ("_server" in riffile):
+    spinMkvFile = riffile + "/spin_server.mkv"
+    spinRawFile = riffile + "/spin_server.raw"
+elif ("_client" in riffile):
+    spinMkvFile = riffile + "/spin_client.mkv"
+    spinRawFile = riffile + "/spin_client.raw"
 
-imageConvert.mkv2raw(spinMkvFile)
+imageConvert.imgCnvt_mkv2raw(spinMkvFile)
 os.system("../external/splitSpinCamRaw %s" %(spinRawFile))
+fileIO.fileIO_rmFile(spinRawFile)
