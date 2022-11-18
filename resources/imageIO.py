e63d67d7ad14daedc2ba6aad502de287dc1e3ddb
Utkarsh Anand <utkarsh.anand@rapsodo.com>
2022-11-18 10:51:00 +0800

    Log for saving image sequence.

--- resources/imageIO.py
+++ resources/imageIO.py
@@ -1,12 +1,14 @@
 import cv2
+import fileIO
 
 def imgIO_saveImageSequence(gImgStack, path):
     try:
+        fileIO.fileIO_writeToLog("imgIO_saveImageSequence. Saving image sequence to directory %s." %(path), True)
         [row, col, numFrames] = gImgStack.shape
         for frame in range(numFrames):
             fileName = path + "/" + str(frame + 1).zfill(6) + ".png"
             cv2.imwrite(fileName, gImgStack[:, :, frame])
             
     except:
-        print("ERROR. saveImageSequence. Unable to write image stack to %s" %(path))
+        fileIO.fileIO_writeToLog("ERROR. saveImageSequence. Unable to write image stack to %s" %(path), True)
         
