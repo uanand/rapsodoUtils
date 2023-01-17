12483293060ff788180b25a8c53bd2fdc1deaa80
Utkarsh Anand <utkarsh.anand@rapsodo.com>
2023-01-17 17:44:58 +0800

    Remove specific files in a RIF folder.

--- app/deleteFiles_app.py
+++ app/deleteFiles_app.py
@@ -8,18 +8,8 @@ import fileIO
 
 rifFile = sys.argv[1]
 
-deleteFileList = ["cam-0-mkv.raw", "cam-1-mkv.raw", "ingame.mkv_2.raw"]
+deleteFileList = ["cam-0-mkv.raw", "cam-1-mkv.raw", "ingame.mkv_2.raw", "spin_server.raw", "spin_client.raw", "speed_server.raw", "speed_client.raw"]
 
-if (platform.system() == "Windows"):
-    rifFileName          = rifFile.split("\\")[-1]
-else:
-    rifFileName          = rifFile.split("/")[-1]
-    
-if ("_server" in rifFileName):
-    riffTimeStamp = rifFileName.split("_server")[0]
-elif ("_client" in rifFileName):
-    riffTimeStamp = rifFileName.split("_client")[0]
-    
 for deleteFile in deleteFileList:
     fileIO.fileIO_rmFile(rifFile + "/" + deleteFile)
     
